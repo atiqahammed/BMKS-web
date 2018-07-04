65d80d3fc05f0210ea523121b9c039b2bd810ec7
atiqahammed <atiqahammedshamim@gmail.com>
2018-07-04 18:22:36 +0600

    navbar was designed

---
+++ Authentication/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    
+    <link rel="icon" href="images/AuthLogo.png">
+
+    <title>Authentication Application</title>
+    <link rel="stylesheet" href="authentication-css/navbar.css">
+
+
+
+
+
+
+
+
+</head>
+<body>
+
+
+    <div class="navbar navbar-default" style="background-color:rgba(0, 0, 0, .2);">
+    
+        <div class="container">
+            <a href="index.php" class="navbar-brand" style="font-size:32; color:rgba(0, 0, 0, .85); font-weight:bold; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Authentication</a>
+
+            <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse navHeaderCollapse">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li> <a href="#">page1</a> </li>
+                    <li class="dropdown">  
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Page2</a>
+                        <ul class="dropdown-menu">
+                            <li> <a href="#">a</a> </li>
+                            <li> <a href="#">a</a> </li>
+                            <li> <a href="#">a</a> </li>
+                            <li> <a href="#">a</a> </li>
+                            <li> <a href="#">a</a> </li>
+                        
+                        </ul>
+                    
+                    </li>
+                    <li> <a href="#">ok</a> </li>
+                    <li> <a href="#" style="font-size:24; color:green; font-weight:bold; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">SIGN IN</a> </li>
+                
+                </ul>
+                
+            
+            
+            
+            </div>    
+
+        </div>
+
+        
+    
+    
+    
+    </div>
+
+    <p class="brand" style="color:blue;">hello</p>
+
+
+
+    
+</body>
+</html>
