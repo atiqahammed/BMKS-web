e916c7adb3326d8df6843e2abb05070a7a8c4b9d
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2018-12-20 03:49:30 +0600

    end of version 1.0

--- index.php
+++ index.php
@@ -14,6 +14,21 @@
 
 <body>
     
+    <nav class="menu">
+    	<header>Menu <span>X</span></header>
+    	<ol>
+    		<ol class="menu-item"><a href="#">home1</a></ol>
+    		<ol class="menu-item"><a href="#">home2</a></ol>
+    		<ol class="menu-item"><a href="#">home3</a>
+    			<ol class="sub-menu"><a href="">sb1</a></ol>
+    		</ol>
+    		<ol class="menu-item"><a href="#">home4</a></ol>
+    	</ol>
+    </nav>
+
+
+
+
     <p>this page is under construction. :) </p>
     
 
